c43d3eef4a834162ad01d93c7c2bd7046964f17a
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-21 16:17:49 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio 06 - Estrutura Sequencial/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercício_06____Estrutura_Sequencial
+{
+    class Program
+    {
+
+        /// -------> START / MAIN 
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Msg();
+            Console.WriteLine(" ");
+            Console.Write("Digite o valor do lado A: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do lado B: ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do lado C: ");
+            c = double.Parse(Console.ReadLine());
+            Equation(10, 5, 4);
+        }
+        /// -------> FUNCTIONS
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Msg()
+        {
+            Console.WriteLine("Calcule e mostre a área das figuras geométricas.");
+        }
+        /*------------------------------------------------------------------------------------------*/
+        static double Equation(double a, double b, double c)
+        {
+            double area;
+            area = a;
+            return area;
+
+        }
+    }
+    }
